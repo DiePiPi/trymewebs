c73685557d30457bec36e99d60e234a6634e5838
DiePiPi <carybest2002@gmail.com>
2017-02-15 14:17:29 -0800

    initial repo

---
+++ tryme/views.py
@@ -0,0 +1,21 @@
+# try/views.py
+
+from django.http import HttpResponse
+from datetime import datetime
+from django.shortcuts import render
+from .models import Post
+
+
+def hello_world(request):
+    return render(request, 'hello_world.html', {
+        'current_time': str(datetime.now()),
+    })
+
+def nothing_here(request):
+    return HttpResponse("Nothing Here!")
+
+def home(request):
+    post_list = Post.objects.all()
+    return render(request, 'home.html', {
+        'post_list': post_list,
+    })
