693b86bcf846d9428122a8bcb5a97d60c44f9250
DiePiPi <carybest2002@gmail.com>
2017-02-20 00:15:45 -0800

    allow host local and heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '_1==#^oj!m^c(cmu@sdss&pulpa!5o4)_z$h%k9$s@9)tn4#94'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["anewtestwebpage.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
